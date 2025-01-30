df9931312173e570c538f109877894e1c4f4fbbc
Joseph Ajibodu <josephajibodu@gmail.com>
2025-01-30 01:14:20 +0100

    deleting teams and leagues disabled

--- app/Filament/Resources/TeamResource.php
+++ app/Filament/Resources/TeamResource.php
@@ -80,7 +80,7 @@ class TeamResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
